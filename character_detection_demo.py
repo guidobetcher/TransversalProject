a4073914f8e149dbd340ae1d661e8a1bc4b6925b
guidobetcher <guido_betcher@hotmail.com>
2023-02-26 17:14:13 +0100

    demo working fine

--- character_detection_demo.py
+++ character_detection_demo.py
@@ -2,6 +2,22 @@
 import cv2
 import pytesseract
 
+def rotate_image(image, angle):
+    ancho = img.shape[1] #columnas
+    alto = img.shape[0] # filas
+    M = cv2.getRotationMatrix2D((ancho//2,alto//2),angle,1)
+    return cv2.warpAffine(img,M,(ancho,alto))
+
+def char_finder(img, char):
+    ocr_config = r'--psm 10 -c tessedit_char_whitelist=' + char + ' -c min_orientation_margin=180'
+    print(ocr_config)
+    res = pytesseract.image_to_string(img, config=ocr_config)
+    if res:
+        print(res)
+        return True
+    else:
+        return False
+
 # setting the path of pytesseract exe
 # you have to write the location of
 # on which your tesseract was installed
@@ -9,27 +25,30 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 # Now we will read the image in our program
 # you have to put your image path in place of photo.jpg
-img = cv2.imread('IMG_7602.jpg')
+#img = cv2.imread('IMG_7602.jpg')
+img = cv2.imread('Captura de pantalla_2023-02-26_15-51-29.png')
 
 # Our image will read as BGR format,
 # So we will convert in RGB format because
 # tesseract can only read in RGB format
-ancho = img.shape[1] #columnas
-alto = img.shape[0] # filas
-# Rotación
-M = cv2.getRotationMatrix2D((ancho//2,alto//2),83,1)
-img = cv2.warpAffine(img,M,(ancho,alto))
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,2))
-# img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel,iterations=1)
-# img = 255 - img
-# For getting the text and number from image
-ocr_config = r'--psm 10 _char_whitelist=H'
-# ocr_config = r'-c tessedit _char_whitelist=H --psm 10 -l osd'
-print(pytesseract.image_to_string(img, config=ocr_config))
-print("H detected") if pytesseract.image_to_string(img, config=ocr_config) else print("no H detected")
 
-# For displaying the original image
-cv2.imshow("result", img)
-cv2.waitKey(0)
+# Rotación
+rotation_angle = 15
+print(int(90/rotation_angle))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for i in range(int(90/rotation_angle)):
+    # img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,2))
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel,iterations=1)
+    # img = 255 - img
+    # For getting the text and number from image
+    # ocr_config = r'-c tessedit _char_whitelist=H --psm 10 -l osd'
+    if char_finder(img, "H"):
+        print("H detected")
+        # For displaying the original image
+        cv2.imshow("result", img)
+        cv2.waitKey(0)
+        break
+    else:
+        img = rotate_image(img, 25)
+        print("no H detected")
